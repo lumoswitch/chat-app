b1a477b2ea188ca5c80308d093dd54236b489cef
LumosWitch <sofrona.anna@gmail.com>
2020-09-10 17:38:37 +0300

    Chat box created

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 import queryString  from 'query-string';
 import io from 'socket.io-client';
 
+import './Chat.css';
+import InfoBar from '../InfoBar/InfoBar';
+
 let socket;
 
 
@@ -48,9 +51,10 @@ const Chat = ({ location }) => {
     return (
         <div className="outerContainer">
             <div className="container">
-                <input value={message} 
+                <InfoBar room={room} />
+                {/* <input value={message} 
                         onChange={(event) => setMessage(event.target.value)} 
-                        onKeyPress={(event) => event.key === 'Enter' ? sendMessage(event) : null}/>
+                        onKeyPress={(event) => event.key === 'Enter' ? sendMessage(event) : null}/> */}
             </div>
         </div>
     )
--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ io.on('connection', (socket) => {
     });
 
     socket.on('disconnect', () => {
-        console.log('UsSer left!!!');
+        console.log('User left!!!');
     });
 });
 
